e78e6197981c34fb13f10494caf917f7c242b0fe
OsvalDev <osvaworkdev@gmail.com>
2024-12-04 10:57:45 -0600

    fix: static path

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./dist'))
 
 app.get('/', Pages.getIndex);
 app.get('/api/user', controller.getUser);
